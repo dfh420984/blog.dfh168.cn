5a6835c5f4a90313e7e68e6c0828b809f47e1407
dfh420984 <352928736@qq.com>
2018-12-26 17:01:43 +0800

    no message

---
+++ App/Config/mysql_table.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: duanfuhao
+ * Date: 2018/12/26
+ * Time: 14:13
+ */
+return [
+    'admin_table' => 'admin',
+    'category_table' => 'category',
+    'comments_table' => 'comments',
+    'posts_table' => 'posts',
+    'user_table' => 'user',
+];
