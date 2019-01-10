b6755875a7cedc9d6f1fef5b0bc64fc35f30c350
dfh420984 <352928736@qq.com>
2019-01-10 23:48:55 +0800

    no message

---
+++ App/Config/mysql.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: duanfuhao
+ * Date: 2018/12/16
+ * Time: 23:19
+ */
+################ DATABASE CONFIG ##################
+//mysql连接池配置
+return [
+    'host'=>'mysql',
+    'port'=>3306,
+    'user'=>'root',
+    'password'=>'123456',
+    'database'=>'blog',
+    'charset' => 'utf8',
+    'timeout' => 5,
+    'pool_max_num' => 20,
+    'pool_time_out' => 1
+];
