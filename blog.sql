f03f1e57e043deeed989345a12942b73b14c1acc
dfh420984 <352928736@qq.com>
2019-01-17 17:39:49 +0800

    no message

--- blog.sql
+++ blog.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50642
 File Encoding         : 65001
 
-Date: 2018-12-27 17:29:06
+Date: 2019-01-17 17:38:04
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,6 +23,7 @@ CREATE TABLE `admin` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '后台管理员表主键',
   `email` varchar(35) NOT NULL DEFAULT '' COMMENT '管理员邮箱',
   `mobile` char(13) NOT NULL DEFAULT '' COMMENT '管理员手机号',
+  `passwd` varchar(64) NOT NULL DEFAULT '' COMMENT '密码（默认123456hash加密）',
   `head_image` varchar(255) NOT NULL DEFAULT '' COMMENT '用户头像',
   `ip` int(10) NOT NULL DEFAULT '0' COMMENT '管理员登录ip',
   `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '用户状态,0：禁用 ,1.启用，2.删除',
@@ -32,12 +33,7 @@ CREATE TABLE `admin` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `ix_email` (`email`),
   UNIQUE KEY `ix_mobile` (`mobile`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='后台管理员表';
-
--- ----------------------------
--- Records of admin
--- ----------------------------
-INSERT INTO `admin` VALUES ('1', '352928736@qq.com', '13331139424', '', '2130706433', '1', '2018-12-26 03:24:14', '2018-12-26 03:24:14', '2018-12-26 03:24:14');
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='后台管理员表';
 
 -- ----------------------------
 -- Table structure for category
@@ -52,15 +48,7 @@ CREATE TABLE `category` (
   `time_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `time_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='分类表';
-
--- ----------------------------
--- Records of category
--- ----------------------------
-INSERT INTO `category` VALUES ('1', '0', '', 'php', '1', '2018-12-26 03:21:04', '2018-12-26 03:21:04');
-INSERT INTO `category` VALUES ('2', '0', '', 'java', '1', '2018-12-26 03:21:04', '2018-12-26 03:21:04');
-INSERT INTO `category` VALUES ('3', '0', '', 'python', '1', '2018-12-26 03:21:04', '2018-12-26 03:21:04');
-INSERT INTO `category` VALUES ('4', '0', '', 'go', '1', '2018-12-26 03:21:04', '2018-12-26 03:21:04');
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='分类表';
 
 -- ----------------------------
 -- Table structure for comments
@@ -78,14 +66,7 @@ CREATE TABLE `comments` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `posts_id` (`posts_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='帖子评论表';
-
--- ----------------------------
--- Records of comments
--- ----------------------------
-INSERT INTO `comments` VALUES ('1', '1', '1', '0', '顶一个', '1', '2018-12-26 03:14:22', '2018-12-26 03:14:22');
-INSERT INTO `comments` VALUES ('2', '2', '1', '1', '顶楼上', '1', '2018-12-26 03:14:22', '2018-12-26 03:14:22');
-INSERT INTO `comments` VALUES ('3', '3', '1', '0', '非常好', '0', '2018-12-26 03:14:22', '2018-12-26 03:14:22');
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='帖子评论表';
 
 -- ----------------------------
 -- Table structure for posts
@@ -93,23 +74,18 @@ INSERT INTO `comments` VALUES ('3', '3', '1', '0', '非常好', '0', '2018-12-26
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '帖子主键',
-  `user_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '发帖用户id,关联user.id',
+  `admin_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '发帖用户id,关联admin.id',
   `cat_id` int(11) NOT NULL DEFAULT '0' COMMENT '帖子分类id,关联category.id',
   `title` varchar(100) NOT NULL DEFAULT '' COMMENT '帖子标题',
   `slug` varchar(255) NOT NULL DEFAULT '' COMMENT '别名',
+  `image` varchar(255) NOT NULL DEFAULT '' COMMENT '帖子图像',
   `content` text NOT NULL COMMENT '帖子内容',
   `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态,0:待审核,1.上线,2.删除',
   `time_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `time_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='帖子表';
-
--- ----------------------------
--- Records of posts
--- ----------------------------
-INSERT INTO `posts` VALUES ('1', '1', '1', 'easyswoole强大', '', 'easyswoole太强大了，非常好用', '1', '2018-12-26 03:11:56', '2018-12-26 03:11:56');
-INSERT INTO `posts` VALUES ('2', '1', '1', 'swoole太牛逼了', '', 'swoole太牛逼了，杠杆滴', '1', '2018-12-26 03:11:56', '2018-12-26 03:11:56');
+  KEY `user_id` (`admin_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='帖子表';
 
 -- ----------------------------
 -- Table structure for user
@@ -130,17 +106,4 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `ix_email` (`email`),
   UNIQUE KEY `ix_mobile` (`mobile`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='前台用户表';
-
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES ('1', '352928736@qq.com', '13331139424', '', '', '', '2130706433', '1', '2018-12-26 03:19:03', '2018-12-26 03:19:03', '2018-12-26 03:19:03');
-INSERT INTO `user` VALUES ('2', 'duanfuhao@smzdm.com', '15172471345', '', '', '', '2130706433', '1', '2018-12-26 03:19:03', '2018-12-26 03:19:03', '2018-12-26 03:19:03');
-INSERT INTO `user` VALUES ('3', 'duanfuhao@163.com', '13693240212', '', '', '', '2130706433', '1', '2018-12-26 03:19:03', '2018-12-26 03:19:03', '2018-12-26 03:19:03');
-
-ALTER TABLE `posts`
-ADD COLUMN `image`  varchar(255) NOT NULL DEFAULT '' COMMENT '帖子图像' AFTER `slug`;
-
-ALTER TABLE `posts`
-CHANGE COLUMN `user_id` `admin_id`  int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '发帖用户id,关联admin.id' AFTER `id`;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='前台用户表';
